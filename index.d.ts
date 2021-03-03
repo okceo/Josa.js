cb37ece12019cb8b793eeebb501be312cf71fc39
WonbaeLee <gloomygentleman@gmail.com>
2021-03-03 16:32:19 +0900

    add index.d.ts
    support typescript

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+// https://github.com/e-/Josa.js/pull/5 issue PR 미리 적용한 버전
+
+
+import _josa from "josa"
+
+export type JosaFormat = '을/를' | '을' | '를' | '을를' | '은/는' | '은' | '는' | '은는' | '이/가' | '이' | '가' | '이가';
+
+export interface IJosa {
+  r: (word: string, format: JosaFormat) => string,
+  c: (word: string, format: JosaFormat) => string
+}
+
+declare const Josa: IJosa = _josa;
+
+export default Josa 
